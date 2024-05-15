908d10cd9052f26fdd802827d81f1206e505064d
adriancolman <adriangamarra093@gmail.com>
2024-05-15 16:51:59 -0300

    Termine las actividades js

---
+++ JavaScript/arrays/4-ejercicio.js
@@ -0,0 +1,12 @@
+//Crear una función que reciba un array de palabras y devuelva true si todas las palabras terminan con la letra
+//"a" y false si al menos una palabra no termina con la letra "a".
+
+const array = ["carpa", " tormenta", "casa"];
+const sinA = ["pepe", "carlos", "juan"];
+
+function terminanEnA(words) {
+  const terminaEnA = words.every((word) => word.endsWith("a"));
+  return terminaEnA;
+}
+
+console.log(terminanEnA(array));
---
+++ JavaScript/arrays/5-ordenamiento.js
@@ -0,0 +1,21 @@
+//Reciben una lista de números. Deben ordenar los números de menor a mayor según su valor absoluto.
+//Eso quiere decir que los números negativos pierden el signo y se ordenan como si fueran positivos.
+//Por ejemplo, si reciben [5, -10, -2, -25, -7] deberían devolver [2, 5, 7, 10, 25].
+//Pueden usar el método Math.abs(num) para obtener el valor absoluto de un número.
+
+const numbers = [5, -10, -2, -25, -7, 1];
+const absoluteNumbers = [];
+
+function sortAbsoluteNumbers(numbers) {
+  for (let i = 0; i < numbers.length; i++) {
+    const absolute = Math.abs(numbers[i]);
+    console.log(absolute);
+    absoluteNumbers.push(absolute);
+  }
+
+  const sorted = absoluteNumbers.sort((a, b) => a - b);
+
+  return sorted;
+}
+
+console.log(sortAbsoluteNumbers(numbers));
