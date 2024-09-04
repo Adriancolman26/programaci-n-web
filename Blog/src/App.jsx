16a6f607bdf88853eca105b05ee9de60bcd1d0b6
adriancolman <adriangamarra093@gmail.com>
2024-09-04 16:59:23 -0300

    Hice el armado del layout y post

---
+++ Blog/src/App.jsx
@@ -0,0 +1,85 @@
+import React from "react";
+import "./index.css";
+
+function App() {
+  return (
+    <>
+      <header className="header">
+        <h1>Viaje a Concepción, Paraguay</h1>
+      </header>
+
+      <main className="main">
+        <div className="grilla">
+          <div className="Marco">
+            <div className="inner-scroll">
+              <h2 className="Centrado">Mi estadia y Actividades</h2>
+              <p>
+                Mis vacaciones en Concepción, Paraguay, fueron llenas de
+                momentos especiales. Uno de mis favoritos fue ir a nadar en los
+                arroyos y piscinas locales, donde pasaba horas refrescándome y
+                disfrutando con mis primos. También disfruté salir a comer en
+                los restaurantes de la zona, probando diferentes platos y
+                compartiendo buenos momentos con mi familia.
+              </p>
+
+              <p>
+                Visitar las casas de conocidos fue otra parte importante de mis
+                días. Pasábamos tiempo con familiares y amigos, disfrutando de
+                su compañía y fortaleciendo lazos. Además, jugar fútbol y vóley
+                en el campo de mis abuelos y tíos fue una experiencia llena de
+                energía, donde las tardes se llenaban de risas y competición
+                amistosa.
+              </p>
+
+              <p>
+                En resumen, estas vacaciones fueron muy divertidas gracias a
+                estas actividades y, sobre todo, por poder compartirlas con mi
+                familia.
+              </p>
+            </div>
+            <div className="Contenedor_foto">
+              <img src="Adrian Gamarra(1).jpg" className="Foto" alt="" />
+            </div>
+          </div>
+          <div className="Marco">
+            <div className="inner-scroll">
+              <h2 className="Centrado">Mi estadia y Actividades</h2>
+              <p>
+                Mis vacaciones en Concepción, Paraguay, fueron llenas de
+                momentos especiales. Uno de mis favoritos fue ir a nadar en los
+                arroyos y piscinas locales, donde pasaba horas refrescándome y
+                disfrutando con mis primos. También disfruté salir a comer en
+                los restaurantes de la zona, probando diferentes platos y
+                compartiendo buenos momentos con mi familia.
+              </p>
+
+              <p>
+                Visitar las casas de conocidos fue otra parte importante de mis
+                días. Pasábamos tiempo con familiares y amigos, disfrutando de
+                su compañía y fortaleciendo lazos. Además, jugar fútbol y vóley
+                en el campo de mis abuelos y tíos fue una experiencia llena de
+                energía, donde las tardes se llenaban de risas y competición
+                amistosa.
+              </p>
+
+              <p>
+                En resumen, estas vacaciones fueron muy divertidas gracias a
+                estas actividades y, sobre todo, por poder compartirlas con mi
+                familia.
+              </p>
+            </div>
+            <div className="Contenedor_foto">
+              <img src="Adrian Gamarra(1).jpg" className="Foto" alt="" />
+            </div>
+          </div>
+        </div>
+      </main>
+
+      <footer className="footer">
+        <p>Adrian Colman</p>
+      </footer>
+    </>
+  );
+}
+
+export default App;
